e229d2b3cc96e45cb8cda5a37b23dec7c4e18a53
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-07-31 02:30:27 +1200

    added few tests

--- cise_ass1a_worksheet-frontend/src/App.test.tsx
+++ cise_ass1a_worksheet-frontend/src/App.test.tsx
@@ -3,26 +3,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-  test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//default test
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+//test by rendering
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe('Addition', () => {
+  //test by describing
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
   });
 
- 
-          //test by describing
-          it('knows that 2 and 2 make 4', () => {
-            expect(2 + 2).toBe(4);
-          });
+  //second test where calculation not same as result
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(5);
+  });
+}
 
-    describe('Addition', () => {
-
-       //test by rendering
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<App />, div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
-  }
-
-    )
+)
